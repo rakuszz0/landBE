18ba70384964d3cd5a7580412b8595cdabb97873
rakuszz0 <jiwagila023@gmail.com>
2023-02-18 07:25:00 +0700

    done

--- pkg/connection/mysql.go
+++ pkg/connection/mysql.go
@@ -23,7 +23,7 @@ func Database() {
 	var DB_NAME = os.Getenv("DB_NAME")
 	var DB_PORT = os.Getenv("DB_PORT")
 
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s", DB_HOST, DB_USER, DB_PASSWORD,DB_NAME, DB_PORT)
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s", DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT)
 	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
 	if err != nil {
@@ -32,3 +32,5 @@ func Database() {
 
 	fmt.Println("Database Connected!")
 }
+
+// v
